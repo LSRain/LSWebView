eb33d0a6d12f24376a5e758cdfbb647af39cebcb
LSRain <315192042@qq.com>
2017-07-20 14:30:41 +0800

    add webView

---
+++ LSWebView/LSWebView/LSWebView/LSWebView.h
@@ -0,0 +1,75 @@
+//
+//  LSWebView.h
+//  LSWebView
+//
+//  Created by WangBiao on 2017/7/20.
+//  Copyright © 2017年 LSRain. All rights reserved.
+//
+
+/*
+ 感谢作者
+ 以下代码参考自 https://github.com/lczhai/ZLCWebView
+ get from https://github.com/lczhai/ZLCWebView
+ */
+
+#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+
+@class LSWebView;
+@protocol LSWebViewDelegate <NSObject>
+@optional
+- (void)lswebView:(LSWebView *)webview didFinishLoadingURL:(NSURL *)URL;
+- (void)lswebView:(LSWebView *)webview didFailToLoadURL:(NSURL *)URL error:(NSError *)error;
+- (void)lswebView:(LSWebView *)webview shouldStartLoadWithURL:(NSURL *)URL;
+- (void)lswebViewDidStartLoad:(LSWebView *)webview;
+@end
+
+@interface LSWebView : UIView<WKNavigationDelegate, WKUIDelegate, UIWebViewDelegate>
+
+#pragma mark - Public Properties
+
+// delegate
+@property (nonatomic, weak) id <LSWebViewDelegate> delegate;
+
+// The main and only UIProgressView
+@property (nonatomic, strong) UIProgressView *progressView;
+// The web views
+// Depending on the version of iOS, one of these will be set
+@property (nonatomic, strong) WKWebView *wkWebView;
+@property (nonatomic, strong) UIWebView *uiWebView;
+
+#pragma mark - Initializers view
+- (instancetype)initWithFrame:(CGRect)frame;
+
+#pragma mark - Static Initializers
+@property (nonatomic, strong) UIBarButtonItem *actionButton;
+@property (nonatomic, strong) UIColor *tintColor;
+@property (nonatomic, strong) UIColor *barTintColor;
+@property (nonatomic, assign) BOOL actionButtonHidden;
+@property (nonatomic, assign) BOOL showsURLInNavigationBar;
+@property (nonatomic, assign) BOOL showsPageTitleInNavigationBar;
+
+//Allow for custom activities in the browser by populating this optional array
+@property (nonatomic, strong) NSArray *customActivityItems;
+
+#pragma mark - Public Interface
+
+
+// Load a NSURLURLRequest to web view
+// Can be called any time after initialization
+- (void)loadRequest:(NSURLRequest *)request;
+
+// Load a NSURL to web view
+// Can be called any time after initialization
+- (void)loadURL:(NSURL *)URL;
+
+// Loads a URL as NSString to web view
+// Can be called any time after initialization
+- (void)loadURLString:(NSString *)URLString;
+
+
+// Loads an string containing HTML to web view
+// Can be called any time after initialization
+- (void)loadHTMLString:(NSString *)HTMLString;
+
+@end
