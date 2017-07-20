1944bbfde1c532cc6702630cf990d1e9f1ad8f66
LSRain <315192042@qq.com>
2017-07-20 14:33:39 +0800

    add Model

---
+++ LSWebView/LSWebView/LSWebView/LSWebObj.h
@@ -0,0 +1,16 @@
+//
+//  LSWebObj.h
+//  LSWebView
+//
+//  Created by WangBiao on 2017/7/20.
+//  Copyright © 2017年 LSRain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSWebObj : NSObject
+
+@property (nonatomic, copy) NSString *url;
+@property (nonatomic, copy) NSString *title;
+
+@end
---
+++ LSWebView/LSWebView/LSWebView/LSWebObj.m
@@ -0,0 +1,13 @@
+//
+//  LSWebObj.m
+//  LSWebView
+//
+//  Created by WangBiao on 2017/7/20.
+//  Copyright © 2017年 LSRain. All rights reserved.
+//
+
+#import "LSWebObj.h"
+
+@implementation LSWebObj
+
+@end
